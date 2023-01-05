8f101a1a4a3b26465d3c590db276a3fd190330a9
Yassine <YassineLaaziz0@gmail.com>
2023-01-05 14:02:36 +0100

    SSR & SSG & ISR & Not found Page

--- types.ts
+++ types.ts
@@ -1,6 +1,6 @@
 export type Product = {
-    name: string,
-    slug: string,
-    price: number,
-    images: object[]
+  name: string
+  slug: { _type: string; current: string }
+  price: number
+  images: object[]
 }
