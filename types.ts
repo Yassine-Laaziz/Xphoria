2741e4ca5f8fd57aabdb52f67701b0cb86dd17bf
Yassine <YassineLaaziz0@gmail.com>
2023-01-30 08:29:05 +0100

    created and finished slides logic and animation...

--- types.ts
+++ types.ts
@@ -4,11 +4,16 @@ export type Product = {
   slug: { _type: string; current: string }
   price: number
   image: object
-  noBgImages: { color: string; images: object[] }[]
-  sizes: number[]
+  noBgImages: {
+    color: string
+    colorName: string
+    images: object[]
+    sizes: number[]
+  }[]
 }
 
 export type ProductOptions = {
   size: number
   color: string
+  colorName: string
 }
