72a9e0361879981fd699043f0414a5a22af1492c
Yassine <YassineLaaziz0@gmail.com>
2023-12-04 15:10:53 +0100

    quick fix

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,8 +4,11 @@ const connection: { isConnected: boolean } = { isConnected: false }
 
 export async function connect() {
   if (connection.isConnected) return
-  mongoose.set('strictQuery', true)
-  mongoose.set('bufferTimeoutMS', process.env.NODE_ENV ? 30000 : 15000)
-  mongoose.connect(process.env.MONGO_URI)
-  connection.isConnected = true
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    connection.isConnected = true
+  } catch (error) {
+    console.error('MongoDB connection error:', error)
+  }
 }
